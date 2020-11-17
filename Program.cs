42cbfe5225e987d89e2753d34460fab65c84cf8b
Your Name <renato_inovacao@hotmail.com>
2020-11-17 11:17:31 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Atividade - Escreva um algoritmo que receba o número inteiro e exiba sua tabuada até 10
+            Console.WriteLine("Tabuada"); 
+            Console.WriteLine("Digite um número:");
+            int numero = int.Parse(Console.ReadLine());
+            
+            int formula ,contador;
+            for (contador = 1;  contador <=10; ++contador)
+            
+            {
+                formula = numero * contador;
+                Console.WriteLine(numero + "x" + contador + " = " + formula);
+            }
+
+                
+        }
+    }
+}
